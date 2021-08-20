d822d31f30849a0d366c8cc5ab1b725cc74bc13e
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-08-21 01:30:20 +0530

    Delete spring-oneToOne directory

--- spring-oneToOne/src/main/java/springboototo/springoneToOne/Repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package springboototo.springoneToOne.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springboototo.springoneToOne.Entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- spring-oneToOne/src/main/java/springboototo/springoneToOne/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package springboototo.springoneToOne.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springboototo.springoneToOne.Entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
